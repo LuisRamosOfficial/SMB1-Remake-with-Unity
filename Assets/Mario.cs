f65d6a3960f8813f78851e0c7ae410abd7fa30f1
Luis <luisviegasr2+github@gmail.com>
2025-09-02 21:46:25 +0100

    Better Movement

--- Assets/Mario.cs
+++ Assets/Mario.cs
@@ -6,22 +6,25 @@ using UnityEngine;
 
 public class Mario : MonoBehaviour
 {
-
     private readonly GameSettings settings;
     public Rigidbody2D myRigidBody;
     public Animator anim;
     public SpriteRenderer sprite;
-    public float velocity;
-    public float jump;
-
-    public Transform groundCheck;       // Empty GameObject at the player's feet
-    public float checkRadius = 0.2f;    // Size of the circle
-
+    public float maxvelocity = 3f;
+    public float jump  = 11f;
+    public float acceleration = 3f;
+    public float currentSpeedX = 0;
+    public float currentSpeedY;
+    public Transform groundCheck;
+    public float checkRadius = 0.1f;
     public LayerMask whatIsGround; 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
+
     void Start()
     {
-        myRigidBody.freezeRotation = true; // prevents any rotation
+        myRigidBody.gravityScale = 3;
+        myRigidBody.freezeRotation = true;
         
     }
 
@@ -31,12 +34,19 @@ public class Mario : MonoBehaviour
         //Checks
         bool grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
+        if (Input.GetKey(KeyCode.C)) maxvelocity = 5;
+        else maxvelocity = 3;
+        
 
+        //? Jump
         if (grounded && Input.GetKey(KeyCode.X) && !anim.GetBool("isJumping"))
         {
+
+
             myRigidBody.linearVelocity = new Vector2(myRigidBody.linearVelocity.x, jump);
-            anim.SetBool("isRunning", false);
             anim.SetBool("isJumping", true);
+            anim.SetBool("isTurning", false);
+            anim.SetBool("isRunning", false);
         }
 
         if (grounded && !Input.GetKey(KeyCode.X) && anim.GetBool("isJumping"))
@@ -50,42 +60,63 @@ public class Mario : MonoBehaviour
             myRigidBody.linearVelocity = new Vector2(myRigidBody.linearVelocity.x, jump);
         }
     
+    //? Movement
     
         if (Input.GetKey(KeyCode.LeftArrow))
+
         {
+            currentSpeedX = Mathf.MoveTowards(currentSpeedX, -maxvelocity, anim.GetBool("isTurning") ? acceleration * Time.deltaTime* 3 :  acceleration * Time.deltaTime);
             sprite.flipX = true;
             if (!anim.GetBool("isJumping"))
             {
-                
-            anim.SetBool("isRunning", true);
+
+                if (currentSpeedX > 0)
+                {
+                    anim.SetBool("isTurning", true);
+                    anim.SetBool("isRunning", true);
+
+                }
+                else
+                {
+                    anim.SetBool("isRunning", true);
+                    anim.SetBool("isTurning", false);
+                }
             }
-            myRigidBody.linearVelocity = new Vector2(-1f * velocity, myRigidBody.linearVelocity.y);
+            myRigidBody.linearVelocity = new Vector2(1f * currentSpeedX, myRigidBody.linearVelocity.y);
         }
 
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            currentSpeedX = Mathf.MoveTowards(currentSpeedX, maxvelocity, anim.GetBool("isTurning") ? acceleration * Time.deltaTime* 3 :  acceleration * Time.deltaTime);
             sprite.flipX = false;
+
             if (!anim.GetBool("isJumping"))
             {
-            anim.SetBool("isRunning", true);
+
+                if (currentSpeedX < 0)
+                {
+                    anim.SetBool("isTurning", true);
+                    anim.SetBool("isRunning", true);
+                }
+                else
+                {
+                    anim.SetBool("isRunning", true);
+                    anim.SetBool("isTurning", false);
+                }
             }
-            myRigidBody.linearVelocity = new Vector2(1f * velocity, myRigidBody.linearVelocity.y);
+            myRigidBody.linearVelocity = new Vector2(1f * currentSpeedX, myRigidBody.linearVelocity.y);
         }
         else
         {
+            currentSpeedX = Mathf.MoveTowards(currentSpeedX, 0, acceleration * Time.deltaTime * 2);
             if (!anim.GetBool("isJumping"))
             {
-                
-            anim.SetBool("isRunning", false);
+                anim.SetBool("isTurning", false);
+                anim.SetBool("isRunning", false);
             }
-            myRigidBody.linearVelocity = new Vector2(0f, myRigidBody.linearVelocity.y);
+            myRigidBody.linearVelocity = new Vector2(currentSpeedX, myRigidBody.linearVelocity.y);
         }
-
-
-        
-
-
 
         
     }
